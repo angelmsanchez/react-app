89506b629e0758ce1d4ccea34662c961c0402609
angel munoz <angelmsanchez85@gmail.com>
2017-05-11 17:55:31 +0200

    -install react-router -change folders project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import App from './components/core/App';
 
-ReactDOM.render(
+render(
   <App />,
   document.getElementById('root')
-);
+)
